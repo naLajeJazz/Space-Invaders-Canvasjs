df8d71f4cdf2cb04e57057383244050244b640a4
Rodrigo Melo <nalajejazz@gmail.com>
2022-05-18 22:29:50 -0300

    Add sprite animation

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,50 @@
 let canvas = document.querySelector("#canvas");
 let ctx = canvas.getContext("2d");
-canvas.width = 640;
-canvas.height = 480;
+let img = document.getElementById("shipSprite");
+let canvasW=640;
+let canvasH=512;
+let boxX=canvasW/2;
+let boxY=canvasH-64;
+let xIndex = 0;
+canvas.width=canvasW;
+canvas.height=canvasH;
 canvas.style.backgroundColor='black';
 
 
 
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(0, 0, 32, 32);
+///anima imagem
+setInterval(()=>xIndex=32,125)
+setInterval(()=>xIndex=0,250)
+
+
+function loop(){
+    requestAnimationFrame(loop,canvas);
+    
+     
+
+
+    
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.save();
+    
+    
+   
+  
+
+
+ctx.drawImage(img,xIndex,0,32,32,boxX,boxY,32,32);
+
+
+
+ ctx.restore()
+
+ 
+
+    
+
+           
+
+
+
+ 
+}loop()
